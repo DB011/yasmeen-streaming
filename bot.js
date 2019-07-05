0739095d767d2b24f9b1bf8803f5d42dc6fa206b
DB011 <51798746+DB011@users.noreply.github.com>
2019-07-05 05:22:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "587903070743298048") return;
+  if (message.author.id !== "590682006044803103") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
